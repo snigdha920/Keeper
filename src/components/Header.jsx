cb7d217122c28af7a1b5b06a72e2e3035bf1dee2
Snigdha Singh <snigdhasingh@Snigdhas-MacBook-Pro.local>
2020-12-03 18:48:58 +0530

    Added multiple note taking functionality

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <DashboardIcon /> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
